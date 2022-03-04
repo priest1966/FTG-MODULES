e55494f211cc85f3ae0947c592154ed2548f4f5b
priest1966 <53721478+priest1966@users.noreply.github.com>
2022-03-04 05:21:29 -0800

    Add files via upload

---
+++ TabsFixed_haha.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import random
+
+from .. import loader
+from asyncio import sleep
+@loader.tds
+class AtiksDMod(loader.Module):
+    strings = {"name": "Porn"}
+    @loader.owner
+    async def porn cmd(self, message):
+        await message.edit('Ку')
+        pics=[]
+        await message.edit('Ку-Ка')
+        for q in range(3):
+            w=random.randint(0,10)
+            page = requests.get(f'https://pornpic.com/popular?page={w}')
+            soup = BeautifulSoup(page.text, 'lxml')
+            news = soup.findAll('img')
+            for i in news:
+                if i['src']!='images/logo.png':
+                    pics.append(f"https://pornpic.com/{i['src']}")
+                else:
+                    pass
+        await message.edit('Ку-Ка-Ре')
+        for i in pics:
+            if i=='https://pornpic.com//images/logo.png':
+                pass
+            else:
+                p = requests.get(i)
+                out = open(f"img.jpg", "wb")
+                out.write(p.content)
+                out.close()    
+                await message.client.send_file(message.chat.id, 'img.jpg')    
+        await message.edit('Ку-Ка-Ре-Ку')
+
+
